83a1f964b1e2d7a9f4f57aa53347ea5f9e59517f
MarioCif <104808942+MarioCif@users.noreply.github.com>
2023-10-02 09:36:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res)  => {
+    res.send("gsadasda");
+})
+
+app.get('/login', (req,res) => {
+    res.send("Bienvenido");
+})
+
+app.get('/aviones', (req, res) => {
+    res.send("Lista de aviones")
+});
+
+app.get('/autos', (req, res) => {
+    res.send("Lista de autos")
+});
+
+app.get('/motos', (req, res) => {
+    res.send("Lista de motos")
+});
+
+app.update('motos', (req, res) => {
+    
+})
+
+
+app.listen(3000, () => {
+    console.log("hola");
+})
